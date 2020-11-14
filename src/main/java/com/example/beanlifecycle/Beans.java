1589bf12c30fee83236c84b7200fc274e279306d
zahitkaya <zahit_kaya58@hotmail.com>
2020-11-14 12:39:12 +0300

    Initial Commit

--- src/main/java/com/example/beanlifecycle/Beans.java
+++ src/main/java/com/example/beanlifecycle/Beans.java
@@ -4,6 +4,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.*;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -13,68 +14,103 @@ import org.springframework.jmx.export.notification.NotificationPublisherAware;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-public class Beans implements InitializingBean, DisposableBean, BeanNameAware, ResourceLoaderAware,ApplicationContextAware
-, ApplicationEventPublisherAware,MessageSourceAware, NotificationPublisherAware {
+public class Beans implements InitializingBean, DisposableBean, BeanNameAware, ResourceLoaderAware, ApplicationContextAware
+        , ApplicationEventPublisherAware, MessageSourceAware, NotificationPublisherAware, BeanPostProcessor {
 
+    /*Instantiation Bean lifecycle da ilk adım bean manuel olarak oluşturuluyor.
+    IOC XML configirationlarına veya beani tanımlayan anotasyonlara bakar bakar. Bu işlemden sonra beanin instance'tını
+     oluşturur.1
+     Bu işlemden sonra properties injectionla  Bean DI la inject edilir.
 
-    //DisposibleBean'den geliyor. (destroy)
+     */
+    ///////////////////////////////////////////////////////
     @Override
-    public void destroy() throws Exception {
-        System.out.println("Bean sonlandırılıyor. ");
+    public void setBeanName(String s) {
+        System.out.println("AWARE INTERFACE Bean name: " + s);
     }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return "Before initialization of bean";
+    }//2 bu metod bütün bean callbackleri için yeni instance oluşturur.
+
+    //////////////////////////////////////////////////////////
+    //Pre-Initialization
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("Bean başlatılacak (postConstructor)");
+    } //3
+
+    //////////////////////////////////////////////////////////
     //InitialingBean
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("Bean başlatıldı.. InitializingBean interface'inden implement edildi. ");
-    }
-    //Pre-Initialization
-    @PostConstruct
-    public void postConstruct(){
-        System.out.println("Bean başlatılacak (postConstructor)");
-    }
+    }//4
+
+    //////////////////////////////////////////////////////////
+    public void initBean() {
+        System.out.println("Bean kurulumu tamamlandı (Custom initialization)");
+    } //5
+
+    //////////////////////////////////////////////////////////
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return "After initialization of bean";
+    }//6 Bu işlemden sonra bean kullanıma hazır
+
+    /*
+    -------------------------------------------
+     */
 
     @PreDestroy
-    public void preDestroy(){
+    public void preDestroy() {
         System.out.println("Bean bitirilecek (preDestroy)..");
-    }
+    }//7
 
-    public void initBean(){
-        System.out.println("Bean kurulumu tamamlandı (Custom initialization)");
-    }
-    public void destroyBean(){
+    //////////////////////////////////////////////////////////
+    //DisposibleBean'den geliyor. (destroy)
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Bean sonlandırılıyor. ");
+    }//8 Bu metod bean referansına ihtiyaç kalınmadığında çağırılır.
+
+    //////////////////////////////////////////////////////////
+    public void destroyBean() {
         System.out.println("Bean yıkımı tamamlandı. (Custom destruction)");
-    }
+    }//9
+
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        User user=(User) applicationContext.getBean("user");
+        User user = (User) applicationContext.getBean("user");
         user.setName("Zahit");
-        System.out.println("AWARE INTERFACE Application context : "+ user.getName());
+        System.out.println("AWARE INTERFACE Application context : " + user.getName());
     }
 
 
     @Override
     public void setResourceLoader(ResourceLoader resourceLoader) {
-        System.out.println("Dosyamızın kaynağı :"+resourceLoader.getResource("com/sample.txt"));
+        System.out.println("AWARE INTERFACE Dosyamızın kaynağı :" + resourceLoader.getResource("com/sample.txt"));
     }
 
     @Override
     public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
-        System.out.println("AWARE INTERFACE applicationEventPublisher : "+applicationEventPublisher.toString());
+        System.out.println("AWARE INTERFACE applicationEventPublisher : " + applicationEventPublisher.toString());
 
     }
 
     @Override
     public void setMessageSource(MessageSource messageSource) {
-        System.out.println("AWARE INTERFACE messageSource :"+ messageSource.toString());
+        System.out.println("AWARE INTERFACE messageSource :" + messageSource.toString());
     }
 
     @Override
     public void setNotificationPublisher(NotificationPublisher notificationPublisher) {
-        System.out.println("AWARE INTERFACE notificationPublisher :" +notificationPublisher.toString());
+        System.out.println("AWARE INTERFACE notificationPublisher :" + notificationPublisher.toString());
     }
-    @Override
-    public void setBeanName(String s) {
-        System.out.println("AWARE INTERFACE Bean name: "+s);
-    }
+
+
+
+
 }
